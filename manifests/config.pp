fce10c8a9348551111036b88bf97b5ea3edcedc4
Timo Goebel <mail@timogoebel.name>
2014-01-20 21:25:32 +0100

    add user timogoebel

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,33 @@
-package { "git":
-    ensure => "latest"
+package { 'git':
+  ensure => 'latest',
 }
 
-package { "vim":
-    ensure => "latest"
+package { 'vim':
+  ensure => 'latest',
 }
 
-package { "htop":
-    ensure => "latest"
+package { 'htop':
+  ensure => 'latest',
+}
+
+user { 'timogoebel':
+  ensure     => 'present',
+  home       => '/home/timogoebel',
+  shell      => '/bin/bash',
+  managehome => 'true',
+}
+
+file { '/home/timogoebel/.ssh':
+  ensure => 'directory',
+  owner => 'timogoebel',
+  mode => '700',
+  require => User['timogoebel'],
+}
+
+ssh_authorized_key { 'timogoebel-ssh-key':
+  ensure => 'present',
+  type => 'rsa',
+  user => 'timogoebel',
+  key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCZnoPaHvHmIQVqd2fc/w4ZMc+Zpcg2w+xx0LRtlEBKmMohOkio2oMzajAFIdxpvdtmlfXNVO7tUpgPcoPV5UdVWcHkcgzsmIwlcmZsOCExcNlQU6GosK86iyu9qSo2AVn5ttjBRFdCt2AxpLVSZmoUxQ4h5fDXK96oET0fojRzMDk82SbWwlYVJPNHGYDfA/IvxZMLs9G5DJt5vLAfElx8b/eoNQgGmFYgEZZHYiu1JYA5kr4l1MIgwWytWfmvk3Bc4YlSGEwW6Smf0lUh79vnu7DczIxWA2eSlTWDL9cYchcjxDzgOH241ecfW2M+qjLfKsaak076qQO5p/QgTuzR',
+  require => File['/home/timogoebel/.ssh'],
 }
